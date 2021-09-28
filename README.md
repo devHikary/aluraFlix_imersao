dd2811e9e0354551112d7d60c92154ac2792c762
devHikary <wrkkarina@gmail.com>
2021-09-28 15:30:42 -0300

    adicionar o nome do filme

--- README.md
+++ README.md
@@ -7,11 +7,11 @@ Na aula 5, utilizando funções e estrutura condicional foi implementado o botã
 
 ## ⚡Desafios implementados
 - Criar um campo e botão para adicionar a imagem pela tela, e não direto no código
+- Além de colocar a imagem do filme, também adicionar o nome por meio de outro input
 ### 🏗️ Desafios em desenvolvimento
 - Tentar implementar outras versões da estrutura de repetição que fizemos com for, como por exemplo com foreach ou while
 - Criar uma condição para não adicionar filmes repetidos, caso eles já tenham sido adicionados anteriormente
 - Criar um botão para remover um filme na tela
-- Além de colocar a imagem do filme, também adicionar o nome por meio de outro input
 - Guardar todos os filmes adicionados em uma lista/array e percorrer essa lista toda vez que quiser imprimir ou remover o filme
 
 ## 🛠️ Abrir e rodar o projeto
--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 function adicionarFilme() {
   var filmeFavorito = document.getElementById("filme").value;
+  var nomeFilme = document.getElementById("nome").value;
 
   if (filmeFavorito.endsWith(".jpg")) {
-    listarFilmesNaTela(filmeFavorito);
+    listarFilmesNaTela(filmeFavorito, nomeFilme);
   } else{
     console.error("Endereço de filme inválido")
   }
   document.getElementById("filme").value = "";
+  document.getElementById("nome").value = "";
 }
 
-function listarFilmesNaTela(filmeFavorito){
+function listarFilmesNaTela(filmeFavorito, nomeFilme){
   var elementoFilme = "<img src=" + filmeFavorito + ">";
   var elementoLista = document.getElementById("listaFilmes");
-  elementoLista.innerHTML = elementoLista.innerHTML + elementoFilme;
+
+  let imgItem = document.createElement("div")
+  elementoLista.appendChild(imgItem)
+  imgItem.classList.add("img-item")
+
+  imgItem.innerHTML = imgItem.innerHTML + elementoFilme
+
+  let p = document.createElement("p")
+  p.innerText = nomeFilme
+  imgItem.appendChild(p)
 }
+
 
 // https://upload.wikimedia.org/wikipedia/pt/9/92/Rise_of_the_Guardians_poster.jpg
